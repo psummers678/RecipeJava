00523e69f529cbfbd7e77b11c758b94d5d3c57b2
phillip <phillipgsummers@gmail.com>
2022-02-07 13:43:48 +0000

    Wrtn IngredientRetrieved by Id test

--- src/test/java/com/pgs/Recipe/IngredientTest.java
+++ src/test/java/com/pgs/Recipe/IngredientTest.java
@@ -1,13 +1,13 @@
 package com.pgs.Recipe;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import com.pgs.Recipe.model.Ingredient;
 import com.pgs.Recipe.service.IngredientService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
 
 @SpringBootTest
 class IngredientTest {
@@ -15,7 +15,14 @@ class IngredientTest {
     @Autowired
     private IngredientService ingredientService;
 
-    
+    private Ingredient setupIngredient;
+
+    @BeforeTestMethod
+    void setup() {
+        setupIngredient = new Ingredient("First Ingredient", 0.5);
+        ingredientService.create(setupIngredient);
+    }
+
 	@Test
 	void test_IngredientCanBeCreated() {
         Ingredient ingredient = new Ingredient();
@@ -42,6 +49,11 @@ class IngredientTest {
         assertTrue(ingredient.getPrice() == price);
     }
 
+    @Test
+    void test_IngredientCanBeRetrievedById() {
+        Ingredient retrievedIngredient = ingredientService.retrieveById(1);
+        assertTrue(retrievedIngredient.equals(setupIngredient));
+    }
     @Test
     void test_IngredientCanBeUpdated(){
         Ingredient ingredient = new Ingredient("Test", 0.5);
