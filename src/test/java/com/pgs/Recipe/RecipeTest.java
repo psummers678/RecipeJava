0918dc196a1e43881811e11105cbd35080ed427f
phillip <phillipgsummers@gmail.com>
2022-03-18 09:25:01 +0000

    Test written no compiling

---
+++ src/test/java/com/pgs/Recipe/RecipeTest.java
@@ -0,0 +1,30 @@
+package com.pgs.Recipe;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RecipeTest {
+    
+    @Autowired
+    private RecipeService recipeService;
+    private Recipe retrievedRecipe;
+    
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void test_thatABlankRecipeCanBeCreated(){
+        Recipe newRecipe = new Recipe();
+        recipeService.create(newRecipe);
+        assertNotEquals(0, newRecipe.getRecipeId());;
+
+    }
+}
